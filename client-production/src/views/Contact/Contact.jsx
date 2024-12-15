f6d3dac773e736c4fcc7a75853ac9683d39f9f65
Pranjal Kumar <pranjalk449@gmail.com>
2024-12-15 16:54:48 +0530

    update api url

--- client-production/src/views/Contact/Contact.jsx
+++ client-production/src/views/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { API_URL } from '../../App';
 import Navbar from '../../components/Navbar/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSquareFacebook, faTwitter, faLinkedin, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faLinkedin, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import './Contact.sass';
 
@@ -49,29 +49,26 @@ const Contact = () => {
                     <h2 className="contact__subtitle">Get To Know The Developer</h2>
                     <div className="contact__content">
                         <div className="contact__text-container">
-                            <p className="contact__text">Hello! I'm <a href='https://portfolio-six-blue-64.vercel.app/' target='_blank' rel='noopener noreferrer' style={{ color: "rgb(29 191 141)", fontWeight: "400", fontSize: "2rem" }}>Karan Raj</a>, the developer behind <span style={{ color: "rgb(255 0 0)" }}>CampusLocator</span>. The idea for this website came to me during my own struggles to find professors on our expansive campus. I realized how much time and effort could be saved if there was an easy way to locate faculty members and view their schedules.</p>
+                            <p className="contact__text">Hello! I'm <a href='https://portfolio-six-blue-64.vercel.app/' target='_blank' rel='noopener noreferrer' style={{ color: "rgb(29 191 141)", fontWeight: "400", fontSize: "2rem" }}>Pranjal Kumar</a>, the developer behind <span style={{ color: "rgb(255 0 0)" }}>FacultyConnect</span>. The idea for this website came to me during my own struggles to find professors on our expansive campus. I realized how much time and effort could be saved if there was an easy way to locate faculty members and view their schedules.</p>
                             <p className="contact__text">With a passion for leveraging technology to solve real-world problems, I set out to create CampusLocator. My goal was to build a platform that simplifies campus navigation for students and faculty alike, making it easier for everyone to connect and collaborate.</p>
                             <p className="contact__text">I'm always eager to connect with others who share a passion for technology and innovation. Feel free to reach out to me on:</p>
                         </div>
-                        <img src="/photo.jpeg" alt="Your Name" className="contact__image" />
+                        <img src="/photo.jpg" alt="Your Name" className="contact__image" />
                     </div>
                     <div className="contact__social">
-                        <a href='https://github.com/karan2198' target="_blank" rel="noopener noreferrer" className="contact__social-link">
+                        <a href='https://github.com/pranjlee30' target="_blank" rel="noopener noreferrer" className="contact__social-link">
                             <FontAwesomeIcon icon={faGithub} style={{ color: "rgb(29 191 141)", }} />
                         </a>
-                        <a href="https://www.facebook.com/profile.php?id=100056152124501" target="_blank" rel="noopener noreferrer" className="contact__social-link">
-                            <FontAwesomeIcon icon={faSquareFacebook} style={{ color: "rgb(148 112 255)" }} />
-                        </a>
-                        <a href="https://x.com/21ucs144" target="_blank" rel="noopener noreferrer" className="contact__social-link">
+                        <a href="https://x.com/pranjalk449" target="_blank" rel="noopener noreferrer" className="contact__social-link">
                             <FontAwesomeIcon icon={faTwitter} style={{ color: "#1DA1F2" }} />
                         </a>
-                        <a href="https://www.linkedin.com/in/karan-raj-b5823822a/" target="_blank" rel="noopener noreferrer" className="contact__social-link">
+                        <a href="https://www.linkedin.com/in/pranjal-kumar-184913228/" target="_blank" rel="noopener noreferrer" className="contact__social-link">
                             <FontAwesomeIcon icon={faLinkedin} style={{ color: "#0077B5" }} />
                         </a>
-                        <a href="https://www.instagram.com/___karan_raj___/" target="_blank" rel="noopener noreferrer" className="contact__social-link">
+                        <a href="https://www.instagram.com/_luminous_dreamer_/" target="_blank" rel="noopener noreferrer" className="contact__social-link">
                             <FontAwesomeIcon icon={faInstagram} style={{ color: "#E1306C" }} />
                         </a>
-                        <a href="mailto:karanrajgodansa2003@gmail.com" target="_blank" rel="noopener noreferrer" className="contact__social-link">
+                        <a href="mailto:pranjalkee010@gmail.com" target="_blank" rel="noopener noreferrer" className="contact__social-link">
                             <FontAwesomeIcon icon={faEnvelope} style={{ color: "#2470f5", }} />
                         </a>
                     </div>
